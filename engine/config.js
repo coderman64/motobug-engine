96943f36a72eaf6763255fc93b2bd3a5f7bf0ed8
coderman64 <coderman64@gmail.com>
2021-01-23 10:17:00 -0500

    stop skipping menus

--- engine/config.js
+++ engine/config.js
@@ -3,7 +3,7 @@
 var configuration = {
     classicAngles: false,   // snap sonic to 45deg, just like the classics
 	mBlurDefault: true,      // set to false to disable motion blur
-	skipMenus: true,		// set to true to skip the menus and go straight
+	skipMenus: false,		// set to true to skip the menus and go straight
 							// into the gameplay
 	startLevel: 0			// the index of the first level to play
 };
